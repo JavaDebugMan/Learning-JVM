84411bc7d7c0f20c00a3cc7c1b0c744ece6229c1
pengzhe <pengzhepost@sina.com>
2018-11-27 23:36:32 +0800

    自定义ClassLoader

---
+++ src/main/java/com/javaman/classloader/TestClassloader.java
@@ -0,0 +1,28 @@
+package com.javaman.classloader;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+/**
+ * @author pengzhe
+ * @date 2018-11-27 23:22
+ * @description
+ */
+
+public class TestClassloader {
+
+    public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
+
+        MyClassLoader myClassLoader = new MyClassLoader();
+        Class<?> aClass = myClassLoader.loadClass("com.javaman.classloader.HelloWorld");
+        System.out.println(aClass.getClassLoader());
+
+        Object instance = aClass.newInstance();
+        System.out.println(instance);
+
+        Method welcome = aClass.getMethod("welcome");
+        Object invoke = welcome.invoke(instance);
+        System.out.println("Result"+instance);
+
+    }
+}
